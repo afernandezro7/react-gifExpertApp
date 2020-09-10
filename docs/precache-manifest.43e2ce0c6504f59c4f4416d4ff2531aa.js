70a4c6fde33aed41f34e5ebf06c3d593803b02ad
Alberto Fernández <a.fernandez.ro7@gmail.com>
2020-09-09 20:11:02 -0500

    adding docs

---
+++ docs/precache-manifest.43e2ce0c6504f59c4f4416d4ff2531aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcdc1e76351e232dd3e90348f0fcb694",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbfd68d110be42cc9978",
+    "url": "/static/css/main.ca319e4e.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbfd68d110be42cc9978",
+    "url": "/static/js/main.b59f65f7.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
